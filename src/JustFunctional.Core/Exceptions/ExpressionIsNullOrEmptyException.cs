31652ac690fbbfbc670a52081066d7d450db1f4e
Jose Carlos Nuñez Martinez <josecdom94@gmail.com>
2022-03-04 08:39:42 +0100

    Deal with empty expressions (#19)

---
+++ src/JustFunctional.Core/Exceptions/ExpressionIsNullOrEmptyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace JustFunctional.Core
+{
+    [Serializable]
+    public class ExpressionIsNullOrEmptyException : JustFunctionalBaseException
+    {
+        public ExpressionIsNullOrEmptyException() { }
+        public ExpressionIsNullOrEmptyException(string message) : base(message) { }
+        public ExpressionIsNullOrEmptyException(string message, Exception inner) : base(message, inner) { }
+        protected ExpressionIsNullOrEmptyException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
